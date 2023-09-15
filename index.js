7b93574df745fe36842471f6298bc34c147e3bc9
Sreerag Parakkal <145093477+Sreerag-Parakkal@users.noreply.github.com>
2023-09-15 13:14:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// let ele = document.getElementById("heading");
+// console.log(ele);
+
+// let ele = document.getElementsByClassName("sample");
+// console.log(ele);
+
+// let ele = document.querySelector(".sample");
+// console.log(ele);
+
+// let ele = document.querySelectorAll(".sample");
+// console.log(ele);
+
+let ele = document.querySelector(".sample").style.color ="red";
